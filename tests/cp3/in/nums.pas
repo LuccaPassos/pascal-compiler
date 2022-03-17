85bc6788ad1749f260ed5ca5962c0495815fbd4b
Lucca Covre <lucca.covre@industriall.ai>
2022-03-16 21:50:00 -0300

    New test cases

---
+++ tests/cp3/in/nums.pas
@@ -0,0 +1,12 @@
+program nums;
+
+var 
+    inter : integer;
+    another_inter : integer;
+
+begin
+    inter := 10;
+    write('String Table');
+    another_inter := 99; 
+
+end.
---
+++ tests/cp3/in/strings.pas
@@ -0,0 +1,12 @@
+program strings;
+
+var 
+    str : string;
+    another_str : string;
+
+begin
+    str := 'Hello';
+    write('String Table');
+    another_str := 'Nice to see three entries in here';
+
+end.
